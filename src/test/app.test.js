d448c5274fc77e5475b28ad55182ec87f4c827d8
NEHA <n1ehpatel20@gmail.com>
2024-04-15 14:25:40 +0530

    new changes to cypress

--- src/test/app.test.js
+++
@@ -1,8 +0,0 @@
-const App = require('../pages/_app.jsx');
-
-describe('App', () => {
-    it('should render without crashing', () => {
-        const wrapper = shallow(<App />)
-        expect(wrapper).toMatchSnapshot()
-    })
-})
